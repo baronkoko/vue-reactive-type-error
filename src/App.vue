f0e6091a5e1ec7abcba9736941d3c1e89f2b2cc6
baronkoko <rafaienko.o@gmail.com>
2021-08-13 14:43:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="slider"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Swiper, SwiperOptions } from "swiper";
+
+export default defineComponent({
+  name: "App",
+  data: (): {
+    options: SwiperOptions;
+  } => ({
+    options: {},
+  }),
+  mounted() {
+    new Swiper(".slider", this.options);
+  },
+});
+</script>
